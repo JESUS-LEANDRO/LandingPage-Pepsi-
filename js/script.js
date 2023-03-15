3a47602e104ed5d1c3f1dcca08d53126f25166b7
LEANDRO <44475462+JESUS-LEANDRO@users.noreply.github.com>
2023-03-15 14:59:38 -0300

    projeto para treinar o basico do javascript para mudar cor layout

---
+++ js/script.js
@@ -0,0 +1,7 @@
+    function imgSlider(anything){
+        document.querySelector('.pepsi').src = anything;
+    }
+    function chageBgcolor(color){
+        const body = document.querySelector('body');
+        body.style.background = color;
+    }
